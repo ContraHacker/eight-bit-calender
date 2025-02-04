ab44f964e5a25c7872ba6b37dd6acbe73ae0bf2b
ContraHacker <ifchinar@gmail.com>
2025-02-05 00:55:40 +0530

    highlight current date & cleanup code

--- src/main.ts
+++ src/main.ts
@@ -1,83 +1,89 @@
-import './style.css';
+import "./style.css";
 
 let year = new Date().getFullYear();
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1 style = 'text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.7);'>${year}</h1>
     <div class="calendar" id="calender">
     </div>
   </div>
-`
+`;
 
 function setup(calendar: HTMLElement) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const month_containers = months.map((month, i) => {
+    const element = document.createElement("div");
 
-  const month_containers = months.map(month => {
-
-    const element = document.createElement('div');
-
-    const month_name = document.createElement('h2');
-
+    const month_name = document.createElement("h2");
     month_name.innerHTML = month;
-    month_name.className = 'month-name';
+    month_name.className = "month-name";
 
     element.append(month_name);
 
-    return element;
+    element.append(generate_month(element, i));
 
+    return element;
   });
 
-  month_containers.forEach((month_container, i) => generate_month(month_container, i));
+  month_containers.forEach((month) => calendar.append(month));
 
-  month_containers.forEach(container => calendar.appendChild(container));
+  const today_string = new Date().toISOString().split("T")[0];
+  const today = document.getElementById(today_string);
 
-};
+  today!.classList.add("today");
+}
 
 function generate_month(container: HTMLElement, month_index: number) {
+  const month = document.createElement("div");
 
-  const div = document.createElement('div');
+  month.className = "month";
 
-  div.className = 'month';
+  ["s", "m", "t", "w", "t", "f", "s"].forEach((d) => {
+    const day = document.createElement("span");
 
-  ['s', 'm', 't', 'w', 't', 'f', 's'].forEach(d => {
-
-    const day = document.createElement('span');
-
-    day.className = 'day';
+    day.className = "day";
     day.innerHTML = d;
 
-    div.appendChild(day);
-
+    month.append(day);
   });
 
   const offset = new Date(year, month_index, 1).getDay();
 
   for (let x = 0; x < offset; x++) {
-
-    const date = document.createElement('span');
-
-    div.appendChild(date);
-
+    const date = document.createElement("span");
+    month.append(date);
   }
 
   const num_days = new Date(year, month_index + 1, 0).getDate();
 
   for (let x = 1; x <= num_days; x++) {
+    const date = document.createElement("span");
 
-    const date = document.createElement('span');
+    date.className = "date";
+    date.innerHTML = x.toString().padStart(2, "0");
+    date.id = `${year}-${(month_index + 1).toString().padStart(2, "0")}-${x.toString().padStart(2, "0")}`;
 
-    date.className = 'date';
-    date.innerHTML = x.toString().padStart(2, '0');
-    date.style.backgroundColor = `var(--green-${Math.floor(Math.random() * (10 - 5)) + 4}00)`
-
-    div.appendChild(date);
-
+    month.append(date);
   }
 
-  container.appendChild(div);
+  container.append(month);
 
+  return month;
 }
 
-setup(document.getElementById('calender')!);
+setup(document.getElementById("calender")!);
